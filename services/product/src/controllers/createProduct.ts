c898f375e0bd0fc47e0c850b8481d935b782ed22
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2025-03-09 16:26:14 +0600

    added product service

---
+++ services/product/src/controllers/createProduct.ts
@@ -0,0 +1,54 @@
+import { INVENTORY_URL } from "@/config";
+import prisma from "@/prisma";
+import { ProductCreateDTOSchema } from "@/schemas";
+import axios from "axios";
+import { NextFunction, Request, Response } from "express";
+
+const createProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // validate the request body
+    const parsedBody = ProductCreateDTOSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      res.status(400).json({ error: parsedBody.error.errors });
+      return;
+    }
+
+    // check if product with the same sku already exists
+    const existingProduct = await prisma.product.findUnique({
+      where: { sku: parsedBody.data.sku },
+    });
+    if (existingProduct) {
+      res
+        .status(400)
+        .json({ error: "Product with the same SKU already exists" });
+      return;
+    }
+
+    // create product
+    const product = await prisma.product.create({ data: parsedBody.data });
+    console.log("Product created successfully", product.id);
+
+    // create inventory record for the product
+    const { data: inventory } = await axios.post(
+      `${INVENTORY_URL}/inventories`,
+      { productId: product.id, sku: product.sku }
+    );
+    console.log("Inventory created successfully", inventory.id);
+
+    // update product and store inventory id
+    await prisma.product.update({
+      where: { id: product.id },
+      data: { inventoryId: inventory.id },
+    });
+
+    res.status(201).json({ ...product, inventoryId: inventory.id });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default createProduct;
---
+++ services/product/src/controllers/getProducts.ts
@@ -0,0 +1,25 @@
+import prisma from "@/prisma";
+import { NextFunction, Request, Response } from "express";
+
+const getProducts = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const products = await prisma.product.findMany({
+      select: {
+        id: true,
+        sku: true,
+        name: true,
+        price: true,
+        inventoryId: true,
+      },
+    });
+
+    // TODO: Implement pagination
+    // TODO: Implement filtering
+
+    res.json({ products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default getProducts;
